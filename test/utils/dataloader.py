f7c567adbb14819e68c3ccb3a2de6b113eff5d63
msjun23 <msjun23@gmail.com>
2022-11-04 16:39:54 +0900

    Update dataloader for each datasets by tonic

--- test/utils/dataloader.py
+++ test/utils/dataloader.py
@@ -16,11 +16,12 @@ def NMNIST_loader(batch_size=128):
     # If no event occurs within a neighbourhood of 1 pixel across filter_time microseconds(us), 
     # the event is filtered. Smaller filter_time will filter more events.
     frame_transform = transforms.Compose([transforms.Denoise(filter_time=10000), 
-                                         transforms.ToFrame(sensor_size=sensor_size, time_window=1000)])
+                                          transforms.ToFrame(sensor_size=sensor_size, time_window=1000)])
     # Augmentation transform
     aug_transform = transforms.Compose([torch.from_numpy, 
                                         torchvision.transforms.RandomRotation([-10,10])])
     
+    # [(x,y,timestamp,polarity), ...]
     trainset = tonic.datasets.NMNIST(save_to='./data', transform=frame_transform, train=True)
     testset  = tonic.datasets.NMNIST(save_to='./data', transform=frame_transform, train=False)
     cached_trainset = CachedDataset(trainset, cache_path='./cache/nmnist/train', transform=aug_transform)
@@ -31,63 +32,134 @@ def NMNIST_loader(batch_size=128):
     
     return trainloader, testloader
 
-class DVSCifar10(Dataset):
-    def __init__(self, root, train=True, transform=None, target_transform=None):
-        self.root = os.path.expanduser(root)
-        self.transform = transform
-        self.target_transform = target_transform
-        self.train = train
-
-    def __getitem__(self, index):
-        """
-        Args:
-            index (int): Index
-        Returns:
-            tuple: (image, target) where target is index of the target class.
-        """
-        data, target = torch.load(self.root + '{}.pt'.format(index))
-
-        if self.transform is not None:
-            data = self.transform(data)
-
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-
-        return data, target.long()
-
-    def __len__(self):
-        return len(os.listdir(self.root))
-
-def CIFAR10DVS_loader(batch_size=128):
-    sensor_size = tonic.datasets.CIFAR10DVS.sensor_size
+def POKERDVS_loader(batch_size=128):
+    sensor_size = tonic.datasets.POKERDVS.sensor_size
     
     frame_transform = transforms.Compose([transforms.Denoise(filter_time=10000), 
                                           transforms.ToFrame(sensor_size=sensor_size, time_window=1000)])
-    
     # Augmentation transform
     aug_transform = transforms.Compose([torch.from_numpy, 
                                         torchvision.transforms.RandomRotation([-10,10])])
     
-    trainset = tonic.datasets.CIFAR10DVS(save_to='./data', transform=frame_transform)
-    # testset  = tonic.datasets.CIFAR10DVS(save_to='./data', transform=frame_transform)
-    cached_trainset = CachedDataset(trainset, cache_path='./cache/cifar10dvs/train', transform=aug_transform)
-    # cached_testset  = CachedDataset(testset, cache_path='./cache/cifar10dvs/test')  # not aug for test set
+    # [(x,y,timestamp,polarity), ...]
+    trainset = tonic.datasets.POKERDVS(save_to='./data', transform=frame_transform, train=True)
+    testset  = tonic.datasets.POKERDVS(save_to='./data', transform=frame_transform, train=False)
+    cached_trainset = CachedDataset(trainset, cache_path='./cache/pokerdvs/train', transform=aug_transform)
+    cached_testset  = CachedDataset(testset, cache_path='./cache/pokerdvs/test')  # not aug for test set
+    
+    trainloader = DataLoader(cached_trainset, batch_size=batch_size, collate_fn=tonic.collation.PadTensors(), shuffle=True)
+    testloader  = DataLoader(cached_testset, batch_size=batch_size, collate_fn=tonic.collation.PadTensors())
+    
+    return trainloader, testloader
+
+def DVSGesture_loader(batch_size=128):
+    sensor_size = tonic.datasets.DVSGesture.sensor_size
+    
+    frame_transform = transforms.Compose([transforms.Denoise(filter_time=10000), 
+                                          transforms.ToFrame(sensor_size=sensor_size, time_window=1000)])
+    # Augmentation transform
+    aug_transform = transforms.Compose([torch.from_numpy, 
+                                        torchvision.transforms.RandomRotation([-10,10])])
+    
+    # [(x,y,timestamp,polarity), ...]
+    trainset = tonic.datasets.DVSGesture(save_to='./data', transform=frame_transform, train=True)
+    testset  = tonic.datasets.DVSGesture(save_to='./data', transform=frame_transform, train=False)
+    cached_trainset = CachedDataset(trainset, cache_path='./cache/dvsgesture/train', transform=aug_transform)
+    cached_testset  = CachedDataset(testset, cache_path='./cache/dvsgesture/test')  # not aug for test set
+    
+    trainloader = DataLoader(cached_trainset, batch_size=batch_size, collate_fn=tonic.collation.PadTensors(), shuffle=True)
+    testloader  = DataLoader(cached_testset, batch_size=batch_size, collate_fn=tonic.collation.PadTensors())
+    
+    return trainloader, testloader
+
+def DVSLip_loader(batch_size=128):
+    sensor_size = tonic.datasets.DVSLip.sensor_size
+    
+    frame_transform = transforms.Compose([transforms.Denoise(filter_time=10000), 
+                                          transforms.ToFrame(sensor_size=sensor_size, time_window=1000)])
+    # Augmentation transform
+    aug_transform = transforms.Compose([torch.from_numpy, 
+                                        torchvision.transforms.RandomRotation([-10,10])])
+    
+    # [(x,y,timestamp,polarity), ...]
+    trainset = tonic.datasets.DVSLip(save_to='./data', transform=frame_transform, train=True)
+    testset  = tonic.datasets.DVSLip(save_to='./data', transform=frame_transform, train=False)
+    cached_trainset = CachedDataset(trainset, cache_path='./cache/dvslip/train', transform=aug_transform)
+    cached_testset  = CachedDataset(testset, cache_path='./cache/dvslip/test')  # not aug for test set
+    
+    trainloader = DataLoader(cached_trainset, batch_size=batch_size, collate_fn=tonic.collation.PadTensors(), shuffle=True)
+    testloader  = DataLoader(cached_testset, batch_size=batch_size, collate_fn=tonic.collation.PadTensors())
+    
+    return trainloader, testloader
+
+def NCALTECH101_loader(batch_size=128):
+    sensor_size = tonic.datasets.NCALTECH101.sensor_size
+    
+    # To frame transform
+    frame_transform = transforms.Compose([transforms.Denoise(filter_time=10000), 
+                                          transforms.ToFrame(sensor_size=sensor_size, time_window=1000)])
+    # Augmentation transform
+    aug_transform = transforms.Compose([torch.from_numpy, 
+                                        torchvision.transforms.RandomRotation([-10,10])])
+    
+    # [(timestamp,x,y,polarity), ...]
+    dataset = tonic.datasets.NCALTECH101(save_to='./data', transform=frame_transform)
+    cached_dataset = CachedDataset(dataset, cache_path='./cache/ncaltech101', transform=aug_transform)
+    
+    # Split into train & test
+    train_size = int(len(cached_dataset) * 0.7)
+    test_size  = int(len(cached_dataset) * 0.3)
+    trainset, testset = torch.utils.data.random_split(cached_dataset, [train_size, test_size])
     
     trainloader = DataLoader(trainset, batch_size=batch_size, collate_fn=tonic.collation.PadTensors(), shuffle=True)
-    # testloader  = DataLoader(cached_testset, batch_size=batch_size, collate_fn=tonic.collation.PadTensors())
+    testloader  = DataLoader(testset, batch_size=batch_size, collate_fn=tonic.collation.PadTensors())
     
-    return trainloader
+    return trainloader, testloader
+
+def ASLDVS_loader(batch_size=128):
+    sensor_size = tonic.datasets.ASLDVS.sensor_size
     
-    # train_dir = 'data/CIFAR10DVS/train'
-    # test_dir  = 'data/CIFAR10DVS/test'
+    # To frame transform
+    frame_transform = transforms.Compose([transforms.Denoise(filter_time=10000), 
+                                          transforms.ToFrame(sensor_size=sensor_size, time_window=1000)])
+    # Augmentation transform
+    aug_transform = transforms.Compose([torch.from_numpy, 
+                                        torchvision.transforms.RandomRotation([-10,10])])
     
-    # trainloader = DataLoader(DVSCifar10(root=train_dir, transform=frame_transform), 
-    #                          batch_size=batch_size, 
-    #                          collate_fn=tonic.collation.PadTensors(), 
-    #                          shuffle=True)
-    # testloader  = DataLoader(DVSCifar10(root=test_dir, transform=frame_transform), 
-    #                          batch_size=batch_size, 
-    #                          collate_fn=tonic.collation.PadTensors(), 
-    #                          shuffle=False)
+    # [(timestamp,x,y,polarity), ...]
+    dataset = tonic.datasets.ASLDVS(save_to='./data', transform=frame_transform)
+    cached_dataset = CachedDataset(dataset, cache_path='./cache/asldvs', transform=aug_transform)
     
-    # return trainloader, testloader
+    # Split into train & test
+    train_size = int(len(cached_dataset) * 0.7)
+    test_size  = int(len(cached_dataset) * 0.3)
+    trainset, testset = torch.utils.data.random_split(cached_dataset, [train_size, test_size])
+    
+    trainloader = DataLoader(trainset, batch_size=batch_size, collate_fn=tonic.collation.PadTensors(), shuffle=True)
+    testloader  = DataLoader(testset, batch_size=batch_size, collate_fn=tonic.collation.PadTensors())
+    
+    return trainloader, testloader
+
+def CIFAR10DVS_loader(batch_size=128):
+    sensor_size = tonic.datasets.CIFAR10DVS.sensor_size
+    
+    # To frame transform
+    frame_transform = transforms.Compose([transforms.Denoise(filter_time=10000), 
+                                          transforms.ToFrame(sensor_size=sensor_size, time_window=1000)])
+    # Augmentation transform
+    aug_transform = transforms.Compose([torch.from_numpy, 
+                                        torchvision.transforms.RandomRotation([-10,10])])
+    
+    # [(timestamp,x,y,polarity), ...]
+    dataset = tonic.datasets.CIFAR10DVS(save_to='./data', transform=frame_transform)
+    cached_dataset = CachedDataset(dataset, cache_path='./cache/cifar10dvs', transform=aug_transform)
+    
+    # Split into train & test
+    train_size = int(len(cached_dataset) * 0.7)
+    test_size  = int(len(cached_dataset) * 0.3)
+    trainset, testset = torch.utils.data.random_split(cached_dataset, [train_size, test_size])
+    
+    trainloader = DataLoader(trainset, batch_size=batch_size, collate_fn=tonic.collation.PadTensors(), shuffle=True)
+    testloader  = DataLoader(testset, batch_size=batch_size, collate_fn=tonic.collation.PadTensors())
+    
+    return trainloader, testloader
